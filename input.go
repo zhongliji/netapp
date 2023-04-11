52ff770aa8cc6430d230cf3258e6d837421d8824
zhongliji <51046005+zhongliji@users.noreply.github.com>
2023-04-11 16:05:30 +0800

    Create input.go

---
+++ input.go
@@ -0,0 +1,29 @@
+package input
+
+import (
+	"fmt"
+	"github.com/zhongliji/dell"
+)
+
+var Operator string
+var Num1 float64
+var Num2 float64
+var C string
+
+func Input() {
+
+	fmt.Println("请输入操作符(+,-,*,/,%):")
+	fmt.Scanln(&Operator)
+
+	fmt.Println("请输入第一个数字:")
+	fmt.Scanln(&Num1)
+
+	fmt.Println("请输入第二个数字:")
+	fmt.Scanln(&Num2)
+
+	D := cal.Cal(Operator, Num1, Num2)
+	fmt.Printf("%v %s %v = %v\n", Num1, Operator, Num2, D)
+
+	fmt.Println("按c继续,按其他键退出")
+	fmt.Scanln(&C)
+}
